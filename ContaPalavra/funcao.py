649a09235fb8f9ab03abcad4db067b54dad7709b
migge <lucasmigge@gmail.com>
2020-04-21 20:14:57 -0300

    Reorganizando arquivos e corrigindo código

--- ContaPalavra/funcao.py
+++ ContaPalavra/funcao.py
@@ -4,23 +4,25 @@ class Processador:
 		if self.texto_eh_valido(texto):
 			self.texto = texto
 
+	def __str__(self):
+		return f"O texto possui um total de {self.conta_caracteres()} caracteres e {self.conta_palavras()} palavras"
 
-	def conta_caracteres(self, texto):
+	def conta_caracteres(self):
 		numero_de_caracteres = 0
-		for caracter in str(texto):
+		for caracter in str(self.texto):
 			numero_de_caracteres += 1
 		return numero_de_caracteres
 
-	def conta_palavra(self, texto):
+	def conta_palavras(self):
 		numero_de_palavra = 0
-		lista_de_palavras = str(texto).split()
+		lista_de_palavras = str(self.texto).split()
 		for palavra in lista_de_palavras:
 			numero_de_palavra += 1
 		return numero_de_palavra
 
 	@staticmethod
 	def texto_eh_valido(texto):
-		if texto == str():
+		if type(texto) == type("string"):
 			return True
 		else:
 			raise ValueError("não use números, pfv")
