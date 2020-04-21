a1c5eaf94648579278f3c5a7cdd58c305f327f28
migge <lucasmigge@gmail.com>
2020-04-21 20:42:01 -0300

    teste de funções do contapalavras

--- ContaPalavra/test_funcao.py
+++ ContaPalavra/test_funcao.py
@@ -1,12 +1,17 @@
-from funcao import *
+from funcao import Processador
 
-def test_conta_caracteres():
-    assert False
+def test_deve_contar_palavras():
+	texto = "eu gosto muito de macaxeira frita com calabresa acebolada"
+	processador = Processador(texto)
+	numero_de_palavras = processador.conta_palavras()
 
+	assert numero_de_palavras == 9
 
-def test_conta_palavra():
-    assert False
+def test_deve_contar_caracteres():
+	texto = "eu gosto muito de macaxeira frita com calabresa acebolada"
+	processador = Processador(texto)
+	numero_de_caracteres = processador.conta_caracteres()
 
+	assert numero_de_caracteres == 57
 
-def test_texto_eh_valido():
-    assert False
+	
